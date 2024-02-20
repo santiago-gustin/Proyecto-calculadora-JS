e9b0180acf2d8dd03cdbc1c46226c4fc141cd718
santiago-gustin <gustinbs@gmail.com>
2024-02-20 13:18:54 -0500

    Se mejora la logica utilizando un for para la lectura de los datos, se incluye el uso de la funcion desvanecerResultado(), se incluye el uso de la funcion mostrarMensajeDeError() la cual hasta la versión pasada no funcionaba bien. En conclusión se logra el funcionamiento deseado. Se puede optimizar un poco más la lógica y mejorar la validación de los datos ingresador al formulario

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-// Envuelve tu código en un evento 'DOMContentLoaded'
+// Evento 'DOMContentLoaded'
 document.addEventListener('DOMContentLoaded', function() {
    
-
+const datosForm = ["name", "docu", "number", "edad", "peso", "altura", "actividad", "femenino", "masculino"];
 const formulario = document.querySelector("#formulario-calculadora");
 const resultado = document.querySelector("#resultado");
 
 formulario.addEventListener("submit", (event)=>{
     event.preventDefault();
-    const name = document.querySelector("#name");
+    let datos = extraerDatos();
+    (datos.every(validarDatos) == true) ? calcularCalorias(datos):mostrarMensajeDeError("Registre todos sus datos por favor.");
+})
+
+function extraerDatos(){
+    //const data = Object.fromEntries(new FormData(event.target));
+    //alert(JSON.stringify(data));
+    
+    /*const name = document.querySelector("#name");
     const docu = document.querySelector("#docu");
     const number = document.querySelector("#number");
     const edad = document.querySelector("#edad");
     const peso = document.querySelector("#peso");
     const altura = document.querySelector("#altura");
     const actividad = document.querySelector("#actividad");
-    const genero = document.querySelector("input[name='genero']:checked");
-    
-    validarDatos(name,genero);
-})
-
-function validarDatos(name,genero){
-    (name.value == "" || docu.value == "" || number.value == "" || edad.value == "" || peso.value == "" || altura.value == "" || actividad.value == "" || genero.value == "") ? mostrarMensajeDeError("Datos faltantes"):calcularCalorias(genero);
+    const genero = document.querySelector("input[name='genero']:checked");*/
+    let datos = [];
+    for (let element of formulario){
+        //console.dir(element);
+        (datosForm.includes(element.id)) ? (element.type == "radio") ? (element.checked == true) ? datos.push(element.value):console.log("Elemento no checkeado"):datos.push(element.value) : console.log("Elemento no pertenece");
+    }
+    console.log(datos);
+    return datos;
 }
 
-function calcularCalorias(genero) {
+function validarDatos(elemento) {
+    return elemento !== ''; // Verifica si el campo no está vacío
+  };
+
+function calcularCalorias(datos) {
 
     const multiplicadorTMB = {
         peso: 10,
@@ -31,16 +44,16 @@ function calcularCalorias(genero) {
         edad: 5
     }
 
-    let calculoCalorias = actividad.value*(multiplicadorTMB.peso*peso.value)+(multiplicadorTMB.altura*altura.value)-(multiplicadorTMB.edad*edad.value);
-    genero.value == "M" ? calculoCalorias = calculoCalorias + 5 : calculoCalorias = calculoCalorias - 161; 
-
-    //totalCalorias.value = `${Math.floor(calculoCalorias)} kcal`;
-    
+    let calculoCalorias = datos[datosForm.indexOf("actividad")]*(multiplicadorTMB.peso*datos[datosForm.indexOf("peso")])+(multiplicadorTMB.altura*datos[datosForm.indexOf("altura")])-(multiplicadorTMB.edad*datos[datosForm.indexOf("edad")]);
+    datos[datosForm.indexOf("femenino")] == "M" ? calculoCalorias += 5 : calculoCalorias -= 161; 
     resultado.innerHTML = `<div class=" card-body d-flex flex-column justify-content-center align-items-center h-100" id="calculo"><h5 class="card-title h2">Calorías requeridas</h5><div class="mb-3 w-100"><input class="form-control text-center" value="${calculoCalorias} kcal" style="font-size: 2rem" disabled></div></div>`;
     resultado.style.display = "block";
     aparecerResultado();
     // Volver a limpiar variables
-
+    setTimeout(() => {
+        desvanecerResultado();
+        formulario.reset();
+    }, 10000);
 }
 
 function mostrarMensajeDeError(msg) {
@@ -54,7 +67,7 @@ function mostrarMensajeDeError(msg) {
     divError.innerHTML = `<span class="alert alert-danger text-center">${msg}</span>`;
 
     resultado.appendChild(divError);
-    
+    resultado.style.display = "block";
     setTimeout(() => {
         divError.remove();
         desvanecerResultado();
@@ -82,7 +95,7 @@ function desvanecerResultado() {
     let distancia = 1;
 
     let id = setInterval(() => {
-        distancia *= 2;
+        distancia *= 1.5;
         resultado.style.top = `${distancia}vh`;
         if (distancia > 100) {
             clearInterval(id);
