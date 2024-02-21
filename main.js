743ba580cb52e04db12e7cfecbddd7768551ebdc
santiago-gustin <gustinbs@gmail.com>
2024-02-20 23:18:23 -0500

    Se optimiza el codigo para la lectura del formulario y se logra el funcionamiento deseado. Se debe continuar con los desarrollos extra, que son la creación de un historial, modificacion del front de la página y posiblmente consumo de Api para recetas

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 // Evento 'DOMContentLoaded'
 document.addEventListener('DOMContentLoaded', function() {
    
-const datosForm = ["name", "docu", "number", "edad", "peso", "altura", "actividad", "femenino", "masculino"];
+//const datosForm = ["name", "docu", "number", "edad", "peso", "altura", "actividad", "femenino", "masculino"];
 const formulario = document.querySelector("#formulario-calculadora");
 const resultado = document.querySelector("#resultado");
+let historial = [];
 
 formulario.addEventListener("submit", (event)=>{
     event.preventDefault();
-    let datos = extraerDatos();
-    (datos.every(validarDatos) == true) ? calcularCalorias(datos):mostrarMensajeDeError("Registre todos sus datos por favor.");
+    const data = Object.fromEntries(new FormData(event.target));
+    //let datos = extraerDatos();
+    let valores = Object.values(data);
+    (valores.every(validarDatos) == true) ? calcularCalorias(data):mostrarMensajeDeError("Registre todos sus datos por favor.");
 })
 
-function extraerDatos(){
-    //const data = Object.fromEntries(new FormData(event.target));
-    //alert(JSON.stringify(data));
-    
-    /*const name = document.querySelector("#name");
-    const docu = document.querySelector("#docu");
-    const number = document.querySelector("#number");
-    const edad = document.querySelector("#edad");
-    const peso = document.querySelector("#peso");
-    const altura = document.querySelector("#altura");
-    const actividad = document.querySelector("#actividad");
-    const genero = document.querySelector("input[name='genero']:checked");*/
+/*function extraerDatos(){
     let datos = [];
     for (let element of formulario){
         //console.dir(element);
@@ -30,23 +22,19 @@ function extraerDatos(){
     }
     console.log(datos);
     return datos;
-}
+}*/
 
 function validarDatos(elemento) {
     return elemento !== ''; // Verifica si el campo no está vacío
   };
 
-function calcularCalorias(datos) {
-
-    const multiplicadorTMB = {
-        peso: 10,
-        altura: 6.25,
-        edad: 5
-    }
-
-    let calculoCalorias = datos[datosForm.indexOf("actividad")]*(multiplicadorTMB.peso*datos[datosForm.indexOf("peso")])+(multiplicadorTMB.altura*datos[datosForm.indexOf("altura")])-(multiplicadorTMB.edad*datos[datosForm.indexOf("edad")]);
-    datos[datosForm.indexOf("femenino")] == "M" ? calculoCalorias += 5 : calculoCalorias -= 161; 
-    let mensaje = `El paciente ${datos[datosForm.indexOf("name")]} identificado con ${datos[datosForm.indexOf("docu")]} NO.${datos[datosForm.indexOf("number")]}, requiere un total de ${calculoCalorias} kcal para el sostenimiento de su TBM`; 
+function calcularCalorias(data) {
+    const multiplicadorTMB = {peso: 10, altura: 6.25, edad: 5};
+    
+    historial.push(data);
+    let calculoCalorias = data.actividad*(multiplicadorTMB.peso*data.peso)+(multiplicadorTMB.altura*data.altura)-(multiplicadorTMB.edad*data.edad);
+    data.genero == "M" ? calculoCalorias += 5 : calculoCalorias -= 161; 
+    let mensaje = `El paciente ${data.name} identificado con ${data.docu} NO.${data.number}, requiere un total de ${calculoCalorias} kcal para el sostenimiento de su TBM.`; 
     resultado.innerHTML = `<div class=" card-body d-flex flex-column justify-content-center align-items-center h-100" id="calculo"><h5 class="card-title h2">Resultados obtenidos</h5><div class="mb-3 w-100" style="height: 100px;"><div class="form-control text-center" style="font-size: 1rem;" contenteditable="false">${mensaje}</div></div></div>`;
     resultado.style.display = "block";
     aparecerResultado();
@@ -62,7 +50,6 @@ function mostrarMensajeDeError(msg) {
     if (calculo) {
         calculo.remove();
     }
-    console.log(msg);
     const divError = document.createElement('div');
     divError.className = 'd-flex justify-content-center align-items-center h-100';
     divError.innerHTML = `<span class="alert alert-danger text-center">${msg}</span>`;
@@ -84,7 +71,7 @@ function aparecerResultado() {
     let distancia = 100;
     let resta = 0.3;
     let id = setInterval(() => {
-        resta *= 1.1;
+        resta *= 1.2;
         resultado.style.top = `${distancia - resta}vh`;
         if (resta > 100) {
             clearInterval(id);
@@ -96,7 +83,7 @@ function desvanecerResultado() {
     let distancia = 1;
 
     let id = setInterval(() => {
-        distancia *= 1.5;
+        distancia *= 1.1;
         resultado.style.top = `${distancia}vh`;
         if (distancia > 100) {
             clearInterval(id);
